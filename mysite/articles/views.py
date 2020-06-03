e5a243a24c35e37fc90da1c28c7142406f8f5d39
Billyzhang1229 <35052899+Billyzhang1229@users.noreply.github.com>
2020-06-03 21:59:02 +0800

    mysite

--- mysite/articles/views.py
+++ mysite/articles/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from . import models
 
 # Create your views here.
+class PostsListView(ListView):
+    model = models.Posts
+    paginate_by = 10
