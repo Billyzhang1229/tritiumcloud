8c2cfd251bd31251ea565833e4ad07ba1ef08198
Billyzhang1229 <35052899+Billyzhang1229@users.noreply.github.com>
2020-06-03 20:17:50 +0800

    upload

--- mysite/userinfo/views.py
+++ mysite/userinfo/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
+from datetime import datetime
+import random
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
@@ -8,5 +10,14 @@ from django.utils.decorators import method_decorator
 class HomeView(generic.TemplateView):
     template_name = 'index.html'
 
+    def get_random_welcome(self):
+        welcome_words = ['こんにちは', 'Welcome', 'Bonjour', 'สวัสดี', 'Хорошо', '欢迎']
+        return random.choice(welcome_words)
+
     def get(self, request):
-        return render(request, self.template_name, {})
+        user_first_name = request.user.first_name
+        user_last_name = request.user.last_name
+        username = request.user.username
+        timenow = datetime.now()
+        welcome_word = self.get_random_welcome()
+        return render(request, self.template_name, locals())
